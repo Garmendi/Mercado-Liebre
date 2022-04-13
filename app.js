b932697f7303afd13761537f85ca3ddbee6e4891
garmmendi <margarcia.mendi@gmail.com>
2022-04-12 21:54:46 -0500

    arreglé rutas

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
 app.listen(process.env.PORT || 3030, () => {
-    console.log('Servidor corriendo en puerto 3000')
+    console.log('Servidor corriendo en puerto 3030')
 })
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
